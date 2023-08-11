63309f310ec0dd77bea7728060576662151fe227
molexandev01~ <molexandev@gmail.com>
2023-08-11 14:19:28 +0300

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -23,4 +23,18 @@ $(function () {
         $(this).next().slideToggle();
     });
 
+
+    // WOW ANIMATION
+
+    wow = new WOW(
+        {
+        boxClass:     'wow',      
+        animateClass: 'animate__animated', 
+        offset:       0,   
+        mobile:       true,
+        live:         true       
+      }
+      )
+      wow.init();
+
 });
